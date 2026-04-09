6c29aea47f9825ea8afd44504b7e34849776b19f
eshika14997 <eshika.patel14997@gmail.com>
2026-04-09 18:48:41 +0530

    Fix typo and enhance description in DashboardHeader
    Corrected a typo in the description and added a new informative paragraph about the website's usefulness.

--- src/DashboardHeader.js
+++ src/DashboardHeader.js
@@ -5,7 +5,10 @@ function DashboardHeader(props) {
     <div>
       <h1>{props.title}</h1>
       <p>{props.subtitle}</p>
-      <p>You can use this website to add you income and expenses.</p>
+      <p>You can use this website to add your income and expenses.</p>
+      <br>
+       <p> This is very useful for students and working people for manage their income and expense, can see their updated balance </p>
+    
     </div>
   );
 }
