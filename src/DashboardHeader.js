9459ac6a7d1834864cd6686fc338231b8c0d34e2
eshika14997 <eshika.patel14997@gmail.com>
2026-04-09 18:44:12 +0530

    Update DashboardHeader.js

--- src/DashboardHeader.js
+++ src/DashboardHeader.js
@@ -6,7 +6,6 @@ function DashboardHeader(props) {
       <h1>{props.title}</h1>
       <p>{props.subtitle}</p>
       <p>You can use this website to add you income and expenses.</p>
-      <p>It is very useful for working people and studnets as it will show the balance amount, you can add income and expenses and the balance will be updated accordingly.</p>
     </div>
   );
 }
